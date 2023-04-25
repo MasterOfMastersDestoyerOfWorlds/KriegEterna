203e5ac0be28e651262d10e9a5f92136adef30c6
Andrew Wollack <andrew.wollack@gmail.com>
2023-04-24 20:36:49 -0400

    batch script

---
+++ batch.bat
@@ -0,0 +1,3 @@
+for /F "tokens=1-6 delims=	" %%A in (CardSheet.tsv) do (
+	gimp-console-2.10.exe -i -b "(script-fu-compile-card \"%%B\" \"%%C\" \"%%D\" \"%%E\" \"%%F\" \"C:\\Users\\Drew\\Documents\\Gimp\\cropped\\%%A.xcf\" \"C:\\Users\\Drew\\Documents\\Gimp\\icons\\embossed\\%%E.xcf\" \"C:\\Users\\Drew\\Documents\\Gimp\\out\\%%A.png\")" -b "(gimp-quit 0)"
+)
