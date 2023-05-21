31d77745539078474ff03e67dc06215e30f28762
Andrew Wollack <andrew.wollack@gmail.com>
2023-05-21 12:28:34 -0400

    fixed batch parallel to work with !

--- batch.bat
+++ batch.bat
@@ -1,5 +1,6 @@
 SET WSPACE=%~dp0
 SET "WSPACE=%WSPACE:\=\\%"
+setlocal enableDelayedExpansion
 for /F "tokens=1-6 delims=	" %%A in (CardSheet.tsv) do (
 	gimp-console-2.10.exe -i -b "(script-fu-compile-card \"%%B\" \"%%C\" \"%%D\" \"%%E\" \"%%F\" \"%WSPACE%cropped\\%%A.xcf\" \"%WSPACE%icons\\embossed\\%%E.xcf\" \"%WSPACE%out\\%%A.png\")" -b "(gimp-quit 0)"
 )
